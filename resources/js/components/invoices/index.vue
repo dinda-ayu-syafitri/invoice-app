472c86991ea34535f2c2622da9c302d94b3b17f3
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-01-25 22:09:43 +0700

    feat: Display all invoices

--- resources/js/components/invoices/index.vue
+++ resources/js/components/invoices/index.vue
@@ -1,6 +1,45 @@
 <template>
-    <div>
-        <h1>Index invoice page</h1>
+    <div class="container mx-auto py-8 px-4">
+        <h1 class="flex justify-center text-3xl mb-8">List of invoices</h1>
+
+        <div class="relative overflow-x-auto">
+            <table
+                class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+            >
+                <thead
+                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                >
+                    <tr>
+                        <th scope="col" class="px-6 py-3">Invoice Number</th>
+                        <th scope="col" class="px-6 py-3">Date</th>
+                        <th scope="col" class="px-6 py-3">Reference</th>
+                        <th scope="col" class="px-6 py-3">Total</th>
+                    </tr>
+                </thead>
+                <tbody
+                    v-for="invoice in invoices"
+                    :key="invoice.id"
+                    v-if="invoices.length > 0"
+                >
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+                    >
+                        <td
+                            scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                            ${{ invoice.number }}
+                        </td>
+                        <td class="px-6 py-4">${{ invoice.date }}</td>
+                        <td class="px-6 py-4">{{ invoice.reference }}</td>
+                        <td class="px-6 py-4">{{ invoice.total }}</td>
+                    </tr>
+                </tbody>
+                <div v-else>
+                    <p>No invoices found</p>
+                </div>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -16,6 +55,7 @@ onMounted(async () => {
 
 const getInvoices = async () => {
     let response = await axios.get("/api/invoices");
-    console.log("response", response);
+
+    invoices.value = response.data.invoices;
 };
 </script>
